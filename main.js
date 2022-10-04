fa3ff452e79df64efe5d580bd0c28c08b8dd558c
Nataly Merezhuk <hello@nataly.dev>
2022-10-04 15:06:18 -0400

    Attribute console .

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ if(url.includes('type')) {
     );
     console.table(selectors);
     console.log(
-      "\n%cTry selecting the first card by its type and changing the color:\n",
+      "\n%cTry selecting the first card by its type and changing the color. Copy the line beow and paste it in the DevTools terminal:\n",
       "font-weight: bold; font-size: .8rem;color: #0C4767;"
     );
     console.log(
@@ -22,7 +22,33 @@ if(url.includes('type')) {
       "font-size: .7rem;color: black;"
     );
 } else if(url.includes('attribute')) {
-    console.log("attributes!");
+    selectors.A = new Selector(
+      "input[placeholder]",
+      'Has "placeholder" attribute.'
+    );
+    selectors.B = new Selector('input[pattern="cat"]', "cat");
+    selectors.C = new Selector('input[pattern~="dog"]', "cat and dog");
+    selectors.D = new Selector('input[pattern|="rat"]', "rat-a-tat");
+    selectors.E = new Selector('input[pattern^="re"]', "repurpose");
+    selectors.F = new Selector('input[pattern$="ty"]', "reality");
+    selectors.G = new Selector('input[pattern*="bamboo"]', "pandas eat bamboo");
+    selectors.E = new Selector(
+      'input[pattern*="JavaScript" i]',
+      "I like javascript"
+    );
+    console.log(
+      `\n%cAttributes`,
+      "color:#0C4767; background: linear-gradient(97deg, #F9CB40 4%, #FF715B 53%, #B7FDFE 100%); font-size:1.5rem; padding:0.15rem 0.25rem; margin: 1rem auto; font-family: Rockwell; border: 2px solid #0dd8d8; border-radius: 4px;font-weight: bold; text-shadow: 1px 1px 1px #00af87bf;"
+    );
+    console.log(
+      `\n%cElements can be selected by their attributes. Here we are selecting the text input by its "placeholder" and "pattern" attributes.`,
+      "font-weight: bold; font-size: .8rem;color: #0C4767;"
+    );
+    console.log(
+      "\n%cTry typing in the phrases in the example column and see what happens 😉\n",
+      "font-weight: bold; font-size: .8rem;color: #0C4767;"
+    );
+    console.table(selectors)
 }else if(url.includes('pseudo-clas')) {
     console.log("pseudo-class!");
 }else if(url.includes('pseudo-element')) {
--- scripts/attribute_selectors.js
+++ scripts/attribute_selectors.js
@@ -1,6 +1,5 @@
 let input = document.getElementById("textInput");
 
 input.onkeyup = (() => {
-    console.log(input.value)
     input.setAttribute("pattern", input.value);
 });
